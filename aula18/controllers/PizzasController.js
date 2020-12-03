299d7adfcafa3533673c1ed1b6530049e230d1c3
Sérgio Moura <smouracalmon@gmail.com>
2020-12-02 22:01:32 -0300

    Criada a view para renderizar dados de uma pizza

--- aula18/controllers/PizzasController.js
+++ aula18/controllers/PizzasController.js
@@ -3,7 +3,8 @@ const pizzas = require('../database/pizzas');
 const PizzasController = {
     getPizza: (req,res) =>{
         let id = req.params.id;
-        res.send(pizzas[id]);
+        // res.send(pizzas[id]);
+        res.render("pizza",{pizza: pizzas[id]});
     }
 }
 
